8b6379e4bef64cef1b9498be6bcf97e0d06b2267
ACER <hendry45xtc@gmail.com>
2022-01-21 23:09:54 +0700

    Constructor

---
+++ src/OOP5/Constructor/Students.java
@@ -0,0 +1,14 @@
+package OOP5.Constructor;
+
+class Students {
+    //field
+    String name;
+    int NPM;
+    final String Address= "Indonesia";
+
+    //constructor
+    Students(String name, int NPM){
+        this.name = name;
+        this.NPM = NPM;
+    }
+}
