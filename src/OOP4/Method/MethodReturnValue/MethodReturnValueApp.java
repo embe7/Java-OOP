b9cf2b99f1c2dec374e9c577113afeb94d0913db
ACER <hendry45xtc@gmail.com>
2022-01-27 16:19:18 +0700

    Method Return Value

---
+++ src/OOP4/Method/MethodReturnValue/MethodReturnValueApp.java
@@ -0,0 +1,9 @@
+package OOP4.Method.MethodReturnValue;
+
+public class MethodReturnValueApp {
+    public static void main(String[] args) {
+        OperasiMatematika penjumlahan = new OperasiMatematika();
+
+        penjumlahan.operasi(100,"+",100);
+    }
+}
