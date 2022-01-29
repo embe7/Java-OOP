7985bdde69b9d4ca749cd7c6efc323776075e462
ACER <hendry45xtc@gmail.com>
2022-01-29 18:02:30 +0700

    Inheritance

---
+++ src/OOP8/Inheritance/Manager.java
@@ -0,0 +1,9 @@
+package OOP8.Inheritance;
+
+class Manager {
+    String name;
+
+    void sayHello(String ParamName){
+        System.out.println("Hello " + ParamName + " My Name Is " + this.name);
+    }
+}
