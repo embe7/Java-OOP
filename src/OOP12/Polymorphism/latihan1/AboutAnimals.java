41990de5a98f26d0050a27ef4d3004cb0ca5fb24
ACER <hendry45xtc@gmail.com>
2022-02-10 02:25:33 +0700

    Polymorphism (latihan1)

---
+++ src/OOP12/Polymorphism/latihan1/AboutAnimals.java
@@ -0,0 +1,13 @@
+package OOP12.Polymorphism.latihan1;
+
+class AboutAnimals {
+    String names;
+    String habitat;
+    int valueLeg;
+
+    AboutAnimals(String NameAnimals,String HabitatAnimal,int LegAnimals){
+        this.names = NameAnimals;
+        this.habitat = HabitatAnimal;
+        this.valueLeg = LegAnimals;
+    }
+}
