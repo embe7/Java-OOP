8560c8728b3fbb697e1a3f532c3169cd98f8834c
embe7 <embe7>
2022-02-19 21:02:54 +0700

    Interface

---
+++ src/OOP23/Interface/Avanza.java
@@ -0,0 +1,11 @@
+package OOP23.Interface;
+
+public class Avanza implements Car{
+    public void drive(){
+        System.out.println("Drive");
+    }
+
+    public int getTire() {
+        return 4;
+    }
+}
