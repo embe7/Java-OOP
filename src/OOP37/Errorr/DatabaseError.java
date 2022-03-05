c195965cb79b8a5be23f082b747ffe5534f47005
embe7 <embe7>
2022-03-05 20:06:46 +0700

    Error

---
+++ src/OOP37/Errorr/DatabaseError.java
@@ -0,0 +1,9 @@
+package OOP37.Errorr;
+
+public class DatabaseError extends Error{
+
+    public DatabaseError(String massage){
+        super(massage);
+    }
+
+}
---
+++ src/OOP37/Errorr/ErrorApp.java
@@ -0,0 +1,24 @@
+package OOP37.Errorr;
+
+/**
+ * Error
+ * - Error adalah jenis exception yang terakhir
+ * - Error adalah sebuah class di java, yang tidak direkomendasikan untuk di try-catch
+ * - Biasanya Error terjadi apabila ada masalah serius, dan sangat tidak direkomendasikan untuk di try-catch
+ * - Artinya direkomendasika untuk mematikan aplikasi
+ * - Contoh, misal jika diawal aplikasi kita tidak bisa terkoneksi ke database, direkomendasikan untuk membuat
+ *   exception jenis error, dan menghentikan aplikasi
+ */
+
+public class ErrorApp {
+    public static void main(String[] args) {
+
+        connectDatabase("admin",null);
+
+    }
+    public static void connectDatabase(String username,String password){
+        if (username == null || password == null){
+            throw new DatabaseError("Tidak bisa koneksi ke database");
+        }
+    }
+}
