975af06a465f94c43a25ec083d436be6dc03120a
embe7 <embe7>
2022-03-25 19:19:51 +0700

    Method Overloading (latihan1)

---
+++ src/OOP4/Method/MethodOverloading/latihan1/SatuApp.java
@@ -0,0 +1,12 @@
+package OOP4.Method.MethodOverloading.latihan1;
+
+public class SatuApp {
+    public static void main(String[] args) {
+        StudentSatu data = new StudentSatu("Java");
+        data.name = "suhendri";
+        data.npm = 18110460;
+        data.kelas = "TI-M1803";
+
+        data.sayStudent();
+    }
+}
